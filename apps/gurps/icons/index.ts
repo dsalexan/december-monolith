8cfe39c1c7d8ba2efbae1e7e94157394721d7a00
Danilo Alexandre <danilo.alexandre@sponte.com.br>
2023-10-09 22:58:07 -0300

    baby steps for content stack

--- apps/gurps/icons/index.ts
+++ apps/gurps/icons/index.ts
@@ -6,6 +6,34 @@ import Defenses from "./defenses"
 
 // define base index of icons
 const base = {
+  // sources
+  gca: `mdi-bookshelf`,
+  no_gca: `no-bookshelf`,
+  no_gca_small: `no-bookshelf-small`,
+  //
+  hit_location: `body-scan-fill`,
+  attribute: `mdi-account`,
+  advantage: `mdi-arrow-up-thick`, // options
+  disadvantage: `mdi-arrow-down-thick`, // options
+  skill: `mdi-hexagon-multiple`,
+  untrained_skill: `mdi-hexagon-multiple-outline`,
+  spell: `spell`,
+  equipment: `mdi-bag-personal`,
+  hp: `mdi-heart`,
+  fp: `mdi-lightning-bolt`,
+  reeling: `heart-down`,
+  tired: `lightning-bolt-down`,
+  //
+  reaction: `mdi-thumb-up`,
+  reaction_negative: `mdi-thumb-down`,
+  //
+  ranged: `mdi-bow-arrow`,
+  melee: `mdi-sword-cross`,
+  // basic
+  to_hit: `3d6`,
+  damage: `critical-strike`,
+  coin: `profit`,
+  //
   ...Moves,
   ...Maneuvers,
   ...Defenses,
